431df567f96c1f067b48da8d752c753fe1defa44
hoooon-97 <psh64354201@gmail.com>
2024-12-09 11:01:32 +0900

    update

---
+++ test.cs
@@ -0,0 +1,9 @@
+using system;
+
+public class program 
+{
+    public static void main(string[] args)
+    {
+        console.writeline("hello world!");
+    }
+}
---
+++ test1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine()); 
+            int result = 0;
+
+            if (X > 0 && Y > 0) {
+                result = 1;
+            } else if (X < 0 && Y > 0) {
+                result = 2;
+            } else if (X < 0 && Y < 0) {
+                result = 3;
+            } else if (X > 0 && Y < 0) {
+                result = 4;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
